843604beacbd6d0f3daf55f41e3d9973dfe7578d
CheaVarin <cheavarin2000@gmail.com>
2022-07-06 21:09:02 +0100

    first commit

---
+++ src/components/SingUp.vue
@@ -0,0 +1,65 @@
+npm install axios
+
+<template>
+<div>
+    <img class="logo" src="../assets/logo.png">
+    <h1>Sing Up</h1>
+    <div class="register">
+        <input type="text" v-model="name" placeholder="Enter Name" >
+        <input type="text" v-model="email" placeholder="Enter Email">
+        <input type="password" v-model="password" placeholder="Enter Password">
+        <button type="submit" v-on:click="signUp">SingUp</button>
+    </div>
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    name: 'SingUp',
+    data(){
+        return{
+            name:'',
+            email:'',
+            password:''
+        }
+    },
+    methods:{
+       async signUp(){
+            console.warn("hello",this.name,this.email,this.password)
+            let result = await axios.post(" http://localhost:3000/users",{   
+                email: this.email,
+                name: this.name,
+                password: this.password
+            });
+            console.warn(result.data);
+            if(result.status==201){
+                alert("Sign Up Done");
+            }
+        }
+    }
+}
+</script>
+<style>
+  .logo{
+    width: 100px;
+  }
+  .register input{
+    width: 300px;
+    height: 40px;
+    padding-left: 20px;
+    display: block;
+    margin-bottom: 30px;
+    margin-left: auto;
+    margin-right: auto;
+    border: 1px solid skyblue;
+  }
+  .register button{
+    width: 320px;
+    height: 40px;
+    border: 1px solid skyblue;
+    color: white;
+    background-color: skyblue;
+    cursor: pointer;
+  }
+</style>
